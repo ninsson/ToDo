4af338c0592ab6caacdd3a821a62d59b97172f0e
Nina Zugaj <ninazugaj25@gmail.com>
2026-05-27 20:15:50 +0200

    Add kdocs to AppDatabase.kt

--- app/src/main/java/com/example/todo/data/AppDatabase.kt
+++ app/src/main/java/com/example/todo/data/AppDatabase.kt
@@ -4,9 +4,14 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 
-// wersja DB zwiększona do 2 (uwaga: fallbackToDestructiveMigration używany przy tworzeniu)
+/**
+ * Baza danych Room dla aplikacji ToDo.
+ *
+ * Zawiera jedną tabelę `tasks` i używa konwerterów JSON dla list załączników i lokalizacji.
+ */
 @Database(entities = [Task::class], version = 2, exportSchema = false)
 @TypeConverters(Converters::class)
 abstract class AppDatabase : RoomDatabase() {
+    /** Dostęp do DAO zadań. */
     abstract fun taskDao(): TaskDao
 }
