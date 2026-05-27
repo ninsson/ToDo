c048a0912d9c7d3d80157df16824648925c92929
Nina Zugaj <ninazugaj25@gmail.com>
2026-05-27 18:27:51 +0200

    Add kdoc to Attachment.kt

--- app/src/main/java/com/example/todo/data/Attachment.kt
+++ app/src/main/java/com/example/todo/data/Attachment.kt
@@ -3,7 +3,12 @@ package com.example.todo.data
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
-// Załącznik przechowywany lokalnie w pamięci aplikacji
+
+/**
+ * Załącznik powiązany z zadaniem.
+ *
+ * Plik jest kopiowany do pamięci aplikacji, a w bazie przechowywana jest lokalna ścieżka.
+ */
 @Serializable
 data class Attachment(
     @SerialName("uri") val localPath: String,
